68f7f9fabdd0d2401da97115e0ae1e7ae9c07039
admin <admin@DESKTOP-1KTB0UQ>
2024-08-07 17:41:53 +0900

    20240807 게시판

--- OhPizza/src/main/java/co/ohpizza/vo/BoardVO.java
+++ OhPizza/src/main/java/co/ohpizza/vo/BoardVO.java
@@ -7,8 +7,8 @@ public class BoardVO {
 	private int boardNo; //board_no
 	private String boardTitle; //board_title
 	private int boardView; //board_view
-	private String boardDate; //boare_date
-	private String review;// review
+	private String boardDate; //board_date
+	private String review; //review
 	private String compaint; //complaint
 	private String answer; //answer
 	private String memId; //mem_id
