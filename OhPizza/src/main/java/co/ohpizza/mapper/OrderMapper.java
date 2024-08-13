6048fb89eddb8d8317a0f7890f2f930475c0bff0
admin <admin@YD02-11>
2024-08-13 17:41:33 +0900

    813j

--- OhPizza/src/main/java/co/ohpizza/mapper/OrderMapper.java
+++ OhPizza/src/main/java/co/ohpizza/mapper/OrderMapper.java
@@ -8,15 +8,23 @@ import co.ohpizza.vo.OrderVO;
 
 public interface OrderMapper {
 	List<OrderVO> selectOrderList(String memId);
-	
+
 	// 장바구니 처음 실행 시 오더 생성
 	int addOrder(String memId);
-	
+
 	int checkOrder(String memId);
-	
+
 	// 해당 아이디의 cart상태 오더 확인
 	OrderVO selectOrder(String memId);
-	
+
 	// 오더 값 넣기
-	int priceOrder(@Param("price")int price, @Param("ordNo")int ordNo);
+	int priceOrder(@Param("price") int price, @Param("ordNo") int ordNo);
+
+	// 결제 중 내역 만들기
+	int addPOrder(OrderVO ovo);
+
+	// 결제 중 내역 번호 가져오기(p상태)
+	OrderVO selectPOrder(String memId);
+
+	int updateOrder(int ordNo);
 }
